805ab293638ba3bec2434343c317ada326fd31ce
Essel Jimmy Kodwo <jimmy.essel@kibo.school>
2023-01-04 23:54:00 +0000

    day 4

--- day_4/isomorphic.py
+++ day_4/isomorphic.py
@@ -7,21 +7,21 @@ class Solution(object):
         """
         self.t = t
         self.s = s
-        pattern_list = list(t)
-        string_list = list(s)
+        t_list = list(t)
+        s_list = list(s)
         x = []
-        for i in pattern_list:
+        for i in t_list:
             if i not in x:
                 x.append(i)
         res = []
-        for i in pattern_list:
+        for i in t_list:
             res.append(x.index(i))
 
         y = []
-        for i in string_list:
+        for i in s_list:
             if i not in y:
                 y.append(i)
         res2 = []
-        for i in string_list:
+        for i in s_list:
             res2.append(y.index(i))
         return res == res2
