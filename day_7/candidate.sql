aabd7bdd7f53ae1ac797ed2f85340f7b5dd830d7
Essel Jimmy Kodwo <jimmy.essel@kibo.school>
2023-01-08 22:01:09 +0000

    day_7

---
+++ day_7/candidate.sql
@@ -0,0 +1,14 @@
+SELECT DISTINCT(candidate_id)
+FROM candidates
+WHERE candidate_id IN
+  (SELECT candidate_id
+  FROM candidates
+  WHERE skill = 'Python')
+AND candidate_id IN
+  (SELECT candidate_id
+  FROM candidates
+  WHERE skill = 'Tableau')
+AND candidate_id IN
+  (SELECT candidate_id
+  FROM candidates
+  WHERE skill = 'PostgreSQL')
